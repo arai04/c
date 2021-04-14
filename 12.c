f125b6e640381aa6778de0f5349216e4929f0adb
arai <>
2021-04-14 20:22:58 +0900

    12

--- 12.c
+++ 12.c
@@ -1,15 +1,17 @@
 # include <stdio.h>
 int main(void){
 
-  int num;
+  int i,j,num;
 
   printf("下向き数字ピラミッドを作ります。\n");
   printf("何段ですか：");
   scanf("%d", &num);
-
-  for (int i = 0; i < num; i++){
-    for (int j = 0; j < num - 2*i; j++){
-      printf("%d", num -i);
+  for (i = 1; i <= num; i++){
+    for (j = 1; j < i; j++){
+      putchar(' ');
+    }
+    for (j = 1; j <= 2 * (num - i) + 1; j++){
+      printf("%d", i % 10);
     }
     puts("");
   }
